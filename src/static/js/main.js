c020d17c99554c74e137378d3fae663b39d36df9
Nella Pražáková <nella.prazakova.023@pslib.cz>
2025-09-01 22:16:38 +0200

    finish: header on all, hamburger menu, transition

---
+++ src/static/js/main.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {
+  if (window.innerWidth > 1080) {
+    scrollFunction();
+  }
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "0rem 4%";
+  } else {
+    document.getElementById("navbar").style.padding = "0.5rem 4%";
+  }
+}
