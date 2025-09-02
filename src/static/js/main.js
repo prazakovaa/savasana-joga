f5a2f3c9bc8cecf4ca7d689e6c2093a1475adf23
Nella Pražáková <nella.prazakova.023@pslib.cz>
2025-09-02 17:13:49 +0200

    finish: page intro, header, articles

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -8,6 +8,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "0rem 4%";
   } else {
-    document.getElementById("navbar").style.padding = "0.5rem 4%";
+    document.getElementById("navbar").style.padding = "0.7rem 4%";
   }
 }
