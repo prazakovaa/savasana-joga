821f3f966bdda0497f0fc305b408e37e3ec544e4
Nella Pražáková <nella.prazakova.023@pslib.cz>
2025-10-02 22:27:14 +0200

    create: sitemap, small changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,30 @@
+const { DateTime } = require("luxon");
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy({ "src/static": "static" });
-    eleventyConfig.addPassthroughCopy({ "src/admin/config.yml": "admin/config.yml" });
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy({ "src/static": "static" });
+  eleventyConfig.addPassthroughCopy({ "src/admin/config.yml": "admin/config.yml" });
 
-
+  // Kolekce "news"
   eleventyConfig.addCollection("news", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/news/*.md").reverse();
   });
 
-    /* show datetime */
-    const { DateTime } = require("luxon");
-
-    eleventyConfig.addFilter("readableDate", dateObj => {
+  // Formátování data
+  eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj).toFormat("dd. MMMM yyyy");
-    });
+  });
 
+  // Sitemap plugin
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://savasana.cz", // nahraď svou doménou
+    },
+  });
 
+  // Výstupní složky
   return {
     dir: {
       input: "src",
@@ -23,3 +32,5 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
+
+
